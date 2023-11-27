e7db8cf10ce56a1349d1fd2e2b1c9b6c99bdbab9
Zhora <151767415+ZhoraManukyan@users.noreply.github.com>
2023-11-27 18:35:51 +0400

    update main.py, add csv-s

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from user import *
+import matplotlib.pyplot as plt
+import pandas as pd
+import csv
 
 class PersonalFinanceManager:
     def __init__(self):
@@ -8,7 +11,10 @@ class PersonalFinanceManager:
     @property
     def get_users_id(self):
         return self.users_id
-
+    @property
+    def get_userslist(self):
+        return self.users
+        
     def create_user(self, user_id, username):
         new_user = User(user_id, username)
         self.users.append(new_user)
@@ -33,47 +39,172 @@ class PersonalFinanceManager:
         else:
             print(f"User '{username}' not found.")
 
+    def create_user(self, user_id, username):
+        # Check if a user with the same user_id already exists
+        if user_id in self.users_id:
+            print(f"User with user_id '{user_id}' already exists. Cannot create a new user.")
+            return None
+
+        # Check if a user with the same username already exists
+        existing_user = self.get_user(username)
+        if existing_user:
+            # Check if the existing user has the same user_id
+            if existing_user.get_user_id() == user_id:
+                print(
+                    f"User with username '{username}' and user_id '{user_id}' already exists. Cannot create a new user.")
+                return None
+
+            # If the user has a different user_id, print a warning
+            print(
+                f"Warning: User with username '{username}' already exists with a different user_id. Creating a new user.")
+
+        # Create a new user
+        new_user = User(user_id, username)
+        self.users.append(new_user)
+        self.users_id.append(new_user.get_user_id())
+        return new_user
+
+
 pf_manager = PersonalFinanceManager()
-# Creating a user
-hovsep = pf_manager.create_user(123, "Hovsep")
-sona = pf_manager.create_user(234, "Sona")
 
-# Creating transactions
-income1 = Income(1000, "2023-01-01", "Salary")
-expense1 = Expense(500, "2023-01-05", "Food")
-one_time_purchase1 = OneTimePurchase(200, "2023-01-10", "Electronics")
+user_ids = []
+# Открываем CSV файл для чтения
+with open('data_users.csv', 'r') as csvfile:
+    # Создаем объект    csv.reader
+    csvreader = csv.reader(csvfile)
+    
+    # Читаем заголовок
+    headers = next(csvreader)
+    
+    # Задаем индекс столбца " "
+    user_id_index = headers.index('user_id')
+    user_income_index = headers.index('income')
+    user_expense_index = headers.index('expense')
+    user_description_index = headers.index('description')
+    user_date_index = headers.index('date')
+    user_name_index = headers.index('username')
+    # Перебираем строки CSV файла
+   # ...
+    for row in csvreader:
+        user_name = row[user_name_index]
+        user_id = row[user_id_index]
+        date = row[user_date_index]
+        description = row[user_description_index]
+        
+        # Преобразование 'income', 'expense' и 'savings' в числа
+        income = float(row[user_income_index])
+        expense = float(row[user_expense_index])
+        
+        # Инициализация объектов
+        income_transaction = Income(income, date, description)
+        expense_transaction = Expense(expense, date, description)
+        
+        # Добавление транзакций к пользователю
+        if user_id not in user_ids:
+            user = pf_manager.create_user(user_id, user_name)
+            user_ids.append(user_id)
+        else:
+            users_id_list = pf_manager.get_users_id
+            index = users_id_list.index(user_id)
+            users_manager = pf_manager.get_userslist
+            user = users_manager[index]
+        
+        user.add_transaction(income_transaction)
+        user.add_transaction(expense_transaction)
+    # ...
 
-income2 = Income(1200, "2023-01-01", "Salary")
-expense2 = Expense(700, "2023-01-05", "Basseyn")
-one_time_purchase2 = OneTimePurchase(600, "2023-01-10", "Clothes")
+# Теперь user_ids содержит все значения из столбца "user_id"
+# Iterating amen user-i ev ir transactions-i vra
+data = []
+for user in pf_manager.users:
+    user_id = user.get_user_id()
+    username = user.username
+    # yndhanur savingsner stexcenq dicti tesqov keyerry klinen datanery valuyenery running savingnery
+    savings_by_date = {}
+    # stecenq popoxakan vor kpahi kutakvac savingnery
+    running_savings = 0
 
-# Adding transactions to the hovsep and sona
-hovsep.add_transaction(income1)
-hovsep.add_transaction(expense1)
-hovsep.add_transaction(one_time_purchase1)
+    # yuraqanchyur transactioni hamar avelacnum enq datanery(tvyalnery)
+    for transaction in user.transactions:
+        temp = True
+        amount, date, description = transaction.get_attributes()
+        income = amount if isinstance(transaction, Income) else 0
+        expense = amount if isinstance(transaction, Expense) else 0
+        saving = income - expense
 
-pf_manager.add_transaction_to_user("Sona", income2)
-pf_manager.add_transaction_to_user("Sona", expense2)
-pf_manager.add_transaction_to_user("Sona", one_time_purchase2)
-# Getting total income, expenses, and savings for the user
-print("HOVSEP")
-total_income = hovsep.get_total_income()
-total_expenses = hovsep.get_total_expenses()
-savings = hovsep.get_savings()
-print("\nTotal Income:", total_income)
-print("Total Expenses:", total_expenses)
-print("Savings:", savings)
+        # Update cumulative savings for the user
+        running_savings += saving
+        # Update savings for the date
+        savings_by_date[date] = running_savings
+        for item in data:
+            if date in item and user_id == item[0]:
+                temp = False
+                index = item.index(date)
+                item[index + 1] += income
+                item[index + 2] += expense
+                item[index + 3] += saving
 
-print("\n\nSONA")
-total_income = sona.get_total_income()
-total_expenses = sona.get_total_expenses()
-savings = sona.get_savings()
-print("\nTotal Income:", total_income)
-print("Total Expenses:", total_expenses)
-print("Savings:", savings)
+        if temp:  # Append enq anum datanery  listum
+            data.append([user_id, username, date, income, expense, saving])
+    for i in range(len(data) - 1):
+        item = data[i]
+        next_item = data[i + 1]
+        if user_id == item[0] and user_id == next_item[0]:
+            next_item[5] += item[5]
+print(data)
 
-# Accessing user transactions
-transactions = pf_manager.get_users_transaction
-id_s = pf_manager.get_users_id
-print("\nUsers's ids: ", id_s)
-print("Users transactions:",transactions)
+# stexcum enq  DataFrame
+columns = ["user_id", "username", "date", "income", "expense", "savings"]
+transaction_df = pd.DataFrame(data, columns=columns)
+
+# DataFrame to CSV file
+transaction_df.to_csv('vizualization_data.csv', index=False)
+
+# DataFrame-i tesqy
+print(transaction_df)
+
+user_ids = transaction_df['user_id'].unique()
+fig, axs = plt.subplots(len(user_ids), 3, figsize=(15, 5 * len(user_ids)))
+
+if len(user_ids) == 1:
+    user_id = user_ids[0]
+    user_data = transaction_df[transaction_df['user_id'] == user_id]
+    username = user_data['username'].iloc[0]
+    axs[0].bar(user_data['date'], user_data['income'], color='green')
+    axs[0].set_title(f'Total Income (Username: {username}, User ID: {user_id})')
+    axs[0].set_xlabel('Date')
+    axs[0].set_ylabel('Income')
+
+    axs[1].bar(user_data['date'], user_data['expense'], color='red')
+    axs[1].set_title(f'Total Expenses (Username: {username}, User ID: {user_id})')
+    axs[1].set_xlabel('Date')
+    axs[1].set_ylabel('Expenses')
+
+    axs[2].plot(user_data['date'], user_data['savings'], marker='o', linestyle='-')
+    axs[2].set_title(f'Savings (Username: {username}, User ID: {user_id})')
+    axs[2].set_xlabel('Date')
+    axs[2].set_ylabel('Savings')
+else:
+    for i, user_id in enumerate(user_ids):
+        user_data = transaction_df[transaction_df['user_id'] == user_id]
+        username = user_data['username'].iloc[0]  # Get the username for the user_id
+        # Plot Total Income
+        axs[i, 0].bar(user_data['date'], user_data['income'], color='green')
+        axs[i, 0].set_title(f'Total Income (Username: {username}, User ID: {user_id})')
+        axs[i, 0].set_xlabel('Date')
+        axs[i, 0].set_ylabel('Income')
+
+        # Plot Total Expenses
+        axs[i, 1].bar(user_data['date'], user_data['expense'], color='red')
+        axs[i, 1].set_title(f'Total Expenses (Username: {username}, User ID: {user_id})')
+        axs[i, 1].set_xlabel('Date')
+        axs[i, 1].set_ylabel('Expenses')
+
+        # Plot Savings
+        axs[i, 2].plot(user_data['date'], user_data['savings'], marker='o', linestyle='-')
+        axs[i, 2].set_title(f'Savings (Username: {username}, User ID: {user_id})')
+        axs[i, 2].set_xlabel('Date')
+        axs[i, 2].set_ylabel('Savings')
+
+plt.tight_layout()
+plt.show()
